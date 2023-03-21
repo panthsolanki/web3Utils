05cb5ea04b68f06b0639a590422dd60617bc51b2
panth <panth.solanki@gameskraft.com>
2023-03-21 19:39:11 +0530

    init

---
+++ components/Solana.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<script lang="ts">
+const solanaWeb3 = require("@solana/web3.js");
+console.log(solanaWeb3);
+</script>
+
+<template>
+  <div>
+    Component: Solana
+  </div>
+</template>
+
+<style scoped></style>
