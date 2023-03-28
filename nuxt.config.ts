d21cbbbaa85458e8b6076a9fb70618d6fddc3886
panthsolanki <panth.solanki@gmail.com>
2023-03-29 01:40:50 +0530

    solana plugin added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,4 +17,21 @@ export default defineNuxtConfig({
       'storeToRefs',
     ],
   },
+  vite: {
+    esbuild: {
+      target: "esnext",
+    },
+    build: {
+      target: "esnext",
+    },
+    optimizeDeps: {
+      include: ["@project-serum/anchor", "@solana/web3.js", "buffer"],
+      esbuildOptions: {
+        target: "esnext",
+      },
+    },
+    define: {
+      "process.env.BROWSER": true,
+    },
+  },
 })
